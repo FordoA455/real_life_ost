ca9161512941491dd8fc280fca0f17a74fe36a22
Milo Lin <milobylin@gmail.com>
2025-08-07 15:54:48 -0700

    Adding main structure
    Signed-off-by: Milo Lin <milobylin@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [song, setSong] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Real Life OST</Text>
+      <Text>Current location:</Text>
+      <Text>Current song: {song}</Text>
+      <TextInput value={song} onChangeText={setSong} placeholder="Enter song:"></TextInput>
     </View>
   );
 }
